8b7197bb579ea06f04705f03414d163123a13007
turovaes <turovaes@gmail.com>
2023-01-31 09:56:12 +0100

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите цифру, обозначающую день недели ");
+
+int.TryParse(Console.ReadLine(), out int DayNumber);
+
+if (DayNumber == 6 || DayNumber == 7)
+{
+    Console.WriteLine("Этот день выходной");
+}
+
+else if (DayNumber < 1 || DayNumber > 7)
+{
+    Console.WriteLine("Вы ввели число, которое не соответсвует дню недели");
+}
+else Console.WriteLine("Этот день не выходной");
