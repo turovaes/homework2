7fc2cf2e5ca87fd92393d2c9d8af965b228f833e
turovaes <turovaes@gmail.com>
2023-01-31 09:00:03 +0100

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите трёхзначное число: ");
+
+int.TryParse(Console.ReadLine(), out int number);
+
+string strnumber = Convert.ToString(number);
+
+int lenght = strnumber.Length;
+
+// Console.WriteLine(lenght);
+
+if (lenght == 3)
+{
+    Console.WriteLine($"Вторая цифра числа {strnumber} - " + strnumber[1]);
+}
+
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+
