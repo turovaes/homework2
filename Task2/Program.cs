f3f473184f04018bb012665e326c18f5cbf269b2
turovaes <turovaes@gmail.com>
2023-01-31 09:06:44 +0100

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+
+int.TryParse(Console.ReadLine(), out int number);
+
+string strnumber = Convert.ToString(number);
+
+int lenght = strnumber.Length;
+
+// Console.WriteLine(lenght);
+
+if (lenght >= 3)
+{
+    Console.WriteLine($"Третья цифра числа {strnumber} - " + strnumber[2]);
+}
+
+else
+{
+    Console.WriteLine("В данном числе отсутсвует третья цифра");
+}
